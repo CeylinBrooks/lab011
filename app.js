0161c8cf8bec386fc39be1f01236d7144da0acb3
ceylinbrooks <ceylinbrooks@gmail.com>
2020-11-02 11:08:33 -0500

    reworking my code

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 'use strict';
 
+//global varibles
+
+var goats = [];
+var totalClicksAllowed = 0;
+var myContainer = document.getElementById('container');
+var imgOne = document.getElementById('img-one');
+var imgTwo = document.getElementById('img-two');
+var imgThree = document.getElementById('img-three');
+
+
+//constructor
+function Goat(name){
+  this.name = name;
+  this.src = `images/${name}.jpg`;
+} 
+
+var myGoat = new Goat('crusin-goat','jpg')
+
+
+//functions
+
+//event handler
+
+//event listener
+
 
 
