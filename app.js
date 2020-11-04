166d6b3d847da38a2d13bc83a17d38fe756a54bb
ceylinbrooks <ceylinbrooks@gmail.com>
2020-11-03 21:59:37 -0500

    HandleClick

--- app.js
+++ app.js
@@ -4,49 +4,72 @@
 
 var goats = [];
 var totalClicksAllowed = 0;
+var clicks = 0;
 var myContainer = document.getElementById('container');
-var imgOne = document.getElementById('img-one');
-var imgTwo = document.getElementById('img-two');
-var imgThree = document.getElementById('img-three');
+var imgOne = document.getElementById('imgone');
+var imgTwo = document.getElementById('imgtwo');
+var imgThree = document.getElementById('imgthree');
 
 
 //constructor
-function Goat(name){
+function Goat(name) {
   this.name = name;
   this.src = `images/${name}.jpg`;
   this.views = 0;
-  this. votes = 0;
+  this.votes = 0;
   goats.push(this);
 }
 
 //functions
 
-function getRandomGoatsIndex(){
+function getRandomGoatsIndex() {
   return Math.floor(Math.random() * goats.length);
 }
+//exectable code
 
-function (){
+new Goat('crusin-goat', 'jpg');
+new Goat('float-your-goat', 'jpg');
+new Goat('goat=away', 'jpg');
+new Goat('goat-out-of-hand', 'jpg');
+new Goat('kissing-goat', 'jpg');
+new Goat('sassy-goat', 'jpg');
+new Goat('smiling-goat');
+new Goat('crusin-goat');
+
+console.log(getRandomGoatsIndex());
+
+
+
+function renderGoats() {
   var goatOne = getRandomGoatsIndex();
   var goatTwo = getRandomGoatsIndex();
   var goatThree = getRandomGoatsIndex();
-  while(goatOne === goatTwo){
+  while (goatOne === goatTwo) {
     goatTwo = getRandomGoatsIndex();
   }
+  imgOneEl.src = goats[goatOne].src;
+  imgOneEl.alt = goats[goatOne].name;
+  
+
+  imgTwoEl.src = goats[goatTwo].src;
+  imgTwoEl.alt = goats[goatTwo].name;
 }
+renderGoats;
+
+console.log(goatOne, goatTwo);
 
 //event handler
+function handleClick(event) {
+  var clickedGoat = event.target.alt;
+  clicks++;
 
-//exectable code
+  renderGoats();
+  if (clicks === totalClicksAllowed) {
+    myContainer.removeEventListener('click', handleClick);
 
-new Goat('crusin-goat','jpg');
-new Goat('float-your-goat','jpg');
-new Goat('goat=away','jpg');
-new Goat('goat-out-of-hand','jpg');
-new Goat('kissing-goat','jpg');
-new Goat('sassy-goat','jpg');
-new Goat('smiling-goat');
-new Goat ('crusin-goat');
+  }
+
+
 //event listener
 
-
-
+myContainer.addEventListener('click', handleClick);
