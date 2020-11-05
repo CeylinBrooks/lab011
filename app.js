4b90a5513a53154811fed99524f1c7287c544253
ceylinbrooks <ceylinbrooks@gmail.com>
2020-11-05 11:20:53 -0500

    OK

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 //global varibles
 
-var goats = [];
+var gifts = [];
 var totalClicksAllowed = 0;
 var clicks = 0;
+var renderQueue = [];
 var myContainer = document.getElementById('container');
 var imgOneEl = document.getElementById('imgone');
 var imgTwoEl = document.getElementById('imgtwo');
@@ -12,59 +13,76 @@ var imgThreeEl = document.getElementById('imgthree');
 
 
 //constructor
-function Goat(name) {
+function Gift(name) {
   this.name = name;
   this.src = `images/${name}.jpg`;
   this.views = 0;
   this.votes = 0;
-  goats.push(this);
+  gifts.push(this);
 }
 
 //functions
 
-function getRandomGoatsIndex() {
-  return Math.floor(Math.random() * goats.length);
+function getRandomGiftsIndex() {
+  return Math.floor(Math.random() * gifts.length);
 }
 //exectable code
 
-new Goat('baconbandages');
-new Goat('begin-weights');
-new Goat('bread-slippers');
-new Goat('cameralensmug');
-new Goat('donut-seeds');
-new Goat('nothing-pck');
-new Goat('pickle-phone');
+new Gift('baconbandages');
+new Gift('begin-weights');
+new Gift('bread-slippers');
+new Gift('cameralensmug');
+new Gift('donut-seeds');
+new Gift('nothing-pck');
+new Gift('pickle-phone');
 
 
-console.log(getRandomGoatsIndex());
+console.log(getRandomGiftsIndex());
+
+// function populateRenderQueue() {
+//   renderQueue = [];
+//   var uniqueGift = getRandomGiftsIndex();
+//   while(!renderQueue.includes(uniqueGift)){
+//     renderQueue.push(uniqueGift);
 
 
 
-function renderGoats() {
-  var goatOne = getRandomGoatsIndex();
-  var goatTwo = getRandomGoatsIndex();
-  var goatThree = getRandomGoatsIndex();
-  while (goatOne === goatTwo) {
-    goatTwo = getRandomGoatsIndex();
+
+
+//   }
+
+// }
+
+
+function renderGifts() {
+  // populateRenderQueue();
+  var giftOne = renderQueue[0];
+  var giftTwo = renderQueue[1];
+  var giftThree = renderQueue[2];
+  while (giftOne === giftTwo) {
+    giftTwo = getRandomGiftsIndex();
   }
-  imgOneEl.src = goats[goatOne].src;
-  imgOneEl.alt = goats[goatOne].name;
+  imgOneEl.src = gifts[giftOne].src;
+  imgOneEl.alt = gifts[giftOne].name;
 
 
-  imgTwoEl.src = goats[goatTwo].src;
-  imgTwoEl.alt = goats[goatTwo].name;
+  imgTwoEl.src = gifts[giftTwo].src;
+  imgTwoEl.alt = gifts[giftTwo].name;
+
+  imgThreeEl.src = gifts[giftThree].src;
+  imgThreeEl.alt = gifts[giftThree].name;
 }
-renderGoats();
+renderGifts();
 
-//console.log(goatOne, goatTwo);
+
 
 //event handler
 
 function handleClick(event) {
-  var clickedGoat = event.target.alt;
+  var clickedGift = event.target.alt;
   clicks++;
 
-  renderGoats();
+  renderGifts();
   if (clicks === totalClicksAllowed) {
     myContainer.removeEventListener('click', handleClick);
 
